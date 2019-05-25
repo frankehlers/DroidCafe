8f3909147b986a3919e21de321c213daaabbc213
Frank Ehlers <ich@frank-ehlers.de>
2019-05-26 00:28:24 +0200

    spinner added

--- app/src/main/java/de/frankehlers/droidcafe/OrderActivity.java
+++ app/src/main/java/de/frankehlers/droidcafe/OrderActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,32 @@ public class OrderActivity extends AppCompatActivity {
                 intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
+
+        // Create mLabelSpinner
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if(spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        //Create array adapter using the string array and default mLabelSpinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        if(spinner != null)
+            spinner.setAdapter(adapter);
+
+        // Specify the layout to use when list of choice appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String spinnerLabel = parent.getItemAtPosition(position).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
     public void onRadioButtonClicked(View view) {
@@ -27,17 +56,17 @@ public class OrderActivity extends AppCompatActivity {
         boolean checked = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.sameday:
-                if(checked)
+                if (checked)
                     displayToast(getString(R.string.same_day_messenger_service));
                 break;
             case R.id.nextday:
-                if(checked)
+                if (checked)
                     displayToast(getString(R.string.next_day_ground_delivery));
                 break;
             case R.id.pickup:
-                if(checked)
+                if (checked)
                     displayToast(getString(R.string.pick_up));
                 break;
             default:
